d091414ce94441db4ad901933c15ccf11c6e4551
Austin Graham <austiingraham@gmail.com>
2023-12-08 03:03:33 -0500

    added generateLogo.js for shape configuration and SVG generation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import necessary modules
 const inquirer = require("inquirer"); // Library for handling user prompts
 const fs = require("fs"); // File system module for reading and writing files
-const { Circle, Square, Triangle } = require("./lib/shapes"); // Import the Circle, Square, and Triangle classes from the 'shapes' module
+const { getShapeConfig, generateSVG } = require("./lib/generateLogo"); // Import getShapeConfig and generateSVG functions from the 'generateLogo' module
 
 // Function to get user input using Inquirer prompts
 function getUserInput() {
@@ -61,67 +61,6 @@ function getUserInput() {
   ]);
 }
 
-// Function to determine the configuration for the selected shape based on user input
-function getShapeConfiguration(userInput) {
-  let shape, textX, textY, fontSize;
-
-  // Switch statement to create the appropriate shape based on user input
-  switch (userInput.shape) {
-    case "Circle":
-      shape = new Circle(); // Create a Circle object
-      textX = "50%"; // Set text X position for Circle
-      textY = "50%"; // Set text Y position for Circle
-      fontSize = 60;
-      break;
-    case "Square":
-      shape = new Square(); // Create a Square object
-      textX = "50%"; // Set text X position for Square
-      textY = "50%"; // Set text Y position for Square
-      fontSize = 60;
-      break;
-    case "Triangle":
-      shape = new Triangle(); // Create a Triangle object
-      textX = "50%"; // Set text X position for Triangle
-      textY = "60%"; // Set text Y position for Triangle
-      fontSize = 50;
-      break;
-    // Throw an error for an invalid shape
-    default:
-      throw new Error("Invalid shape");
-  }
-
-  // Return the configuration for the selected shape
-  return { shape, textX, textY, fontSize };
-}
-
-// Function to generate SVG content based on user input and shape configuration
-function generateSVG(shapeConfig, userInput) {
-  const { shape, textX, textY, fontSize } = shapeConfig;
-  // Define constants for SVG dimensions
-  const svgWidth = 300;
-  const svgHeight = 200;
-  // Set text color based on user input, removing spaces for colors that are entered as 2 words (for example: dark grey)
-  const textColor = userInput.textColor.replace(/\s/g, '');
-  // Set shape color based on user input, removing spaces for colors that are entered as 2 words (for example: light blue)
-  const shapeColor = userInput.shapeColor.replace(/\s/g, '');
-
-  // Apply the user-defined color to the shape
-  shape.setColor(shapeColor);
-
-  // Generate SVG content using template literals
-  const svgContent = `
-    <svg width="${svgWidth}" height="${svgHeight}" xmlns="http://www.w3.org/2000/svg">
-      ${shape.render()}
-      <text x="${textX}" y="${textY}" font-size="${fontSize}" fill="${textColor}" text-anchor="middle" alignment-baseline="middle">
-        ${userInput.text}
-      </text>
-    </svg>
-  `;
-
-  // Return the generated SVG content
-  return svgContent;
-}
-
 // Function to write SVG content to a file
 function writeToFile(fileName, svgContent) {
   // Write the SVG content to a file
@@ -142,7 +81,7 @@ function init() {
   getUserInput()
     .then((userInput) => {
       // Determine the configuration for the selected shape
-      const shapeConfig = getShapeConfiguration(userInput);
+      const shapeConfig = getShapeConfig(userInput);
       // Generate SVG content based on user input and shape configuration
       const svgContent = generateSVG(shapeConfig, userInput);
       // Write the generated SVG content to a file named "logo.svg"
